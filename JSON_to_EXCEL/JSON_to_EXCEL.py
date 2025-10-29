824ed29dd87c9a26fe27ce9d3e36ec32c18c312b
Gergely Fekete <gergely.fekete@hydro.com>
2025-10-29 08:48:34 +0100

    asd

--- JSON_to_EXCEL/JSON_to_EXCEL.py
+++ JSON_to_EXCEL/JSON_to_EXCEL.py
@@ -2,35 +2,32 @@ import json
 import pandas as pd
 import openpyxl
 import os 
+from pathlib import Path
 
 
+current_file=Path(__file__).resolve()
+target_file=current_file.parent / "test.json"
+print(target_file)
+
+def convert_JSON_to_EXCEL (json_filename='test.json'):
+    json_path = os.path.abspath('test.json')
+    excel_path=os.path.abspath('Exports.xlsx')
+    #Load JSON data
+    with open(json_path) as file:
+        data=json.load(file)
 
 
-# Convert to absolute path
-absolute_path = os.path.abspath('test.json')
-#C:\Users\a829052\OneDrive - Norsk Hydro ASA\Dokumentumok\WORK\Python script\JSON_to_EXCEL\test.json
-
-excel_path=os.path.abspath('Exports.xlsx')
-#--------------------------------------------------------------
-
-
-#Load JSON data
-with open(absolute_path) as file:
-    data=json.load(file)
-
-print(data)
-
-# Convert json data to pandas dataframe
-df = pd.DataFrame([data], index=[0]) 
-print(df)
-
-df.to_excel('Exports.xlsx', index=False)
+    # Convert json data to pandas dataframe
+    df = pd.DataFrame([data], index=[0])
+    print(df)
+    print(json_path)
+    if os.path.exists(excel_path):
+        os.remove(excel_path)
+        df.to_excel('Exports.xlsx', index=False)
+#convert_JSON_to_EXCEL()
+    else:
+        df.to_excel('Exports.xlsx', index=False)
 
 
+#convert_JSON_to_EXCEL()
 
-if os.path.exists(absolute_path):
-    os.remove(excel_path)
-    df.to_excel('Exports.xlsx', index=False)
-
-else:
-    df.to_excel('Exports.xlsx', index=False)
--- Main.py
+++ Main.py
@@ -1,6 +1,21 @@
-from API_Handler.API_Handler import download_json
-from Email_SMTP.Email_SMTP import
-from JSON_to_EXCEL.JSON_to_EXCEL import
+import os
+import sys
+
+current_folder=os.path.abspath('Main.py')
+parent_folder=os.path.dirname(current_folder)
+
+#Add the module path to sys.path
+JSON_dir=(os.path.join(parent_folder, 'JSON_to_EXCEL'))
+
+sys.path.append(JSON_dir)
+
+#from JSON_to_EXCEL import convert_JSON_to_EXCEL
+
+#convert_JSON_to_EXCEL()
+
+JSON=os.path.join(JSON_dir,'test.json')
+
+print(JSON_dir)
 
 
 

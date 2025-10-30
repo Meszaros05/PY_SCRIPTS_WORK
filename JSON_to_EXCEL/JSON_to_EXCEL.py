6bccb037820eb0602b4da18e4d3ad07385ba156d
Gergely Fekete <gergely.fekete@hydro.com>
2025-10-30 14:44:48 +0100

    asd

--- JSON_to_EXCEL/JSON_to_EXCEL.py
+++ JSON_to_EXCEL/JSON_to_EXCEL.py
@@ -5,24 +5,28 @@ import os
 from pathlib import Path
 
 
-current_file=Path(__file__).resolve()
-target_file=current_file.parent / "test.json"
-print(target_file)
+#current_file=Path(__file__).resolve()
+#target_file=current_file.parent / "test.json"
+#print(target_file)
 
-def convert_JSON_to_EXCEL (json_filename='test.json'):
-    json_path = os.path.abspath('test.json')
-    excel_path=os.path.abspath('Exports.xlsx')
+def Convert_JSON_to_EXCEL (json_filename='test.json'):
+    #json_path = os.path.abspath('test.json')
+    current_file_JSON=Path(__file__).resolve()
+    target_file_JSON=current_file_JSON.parent / "test.json"
+    current_file_EXCEL=Path(__file__).resolve()
+    target_file_EXCEL=current_file_EXCEL.parent / "Exports.xlsx"
+    #excel_path=os.path.abspath('Exports.xlsx')
     #Load JSON data
-    with open(json_path) as file:
+    with open(target_file_JSON) as file:
         data=json.load(file)
 
 
     # Convert json data to pandas dataframe
     df = pd.DataFrame([data], index=[0])
     print(df)
-    print(json_path)
-    if os.path.exists(excel_path):
-        os.remove(excel_path)
+    #print(target_file)
+    if os.path.exists(target_file_EXCEL):
+        os.remove(target_file_EXCEL)
         df.to_excel('Exports.xlsx', index=False)
 #convert_JSON_to_EXCEL()
     else:
@@ -31,3 +35,4 @@ def convert_JSON_to_EXCEL (json_filename='test.json'):
 
 #convert_JSON_to_EXCEL()
 
+Convert_JSON_to_EXCEL()
